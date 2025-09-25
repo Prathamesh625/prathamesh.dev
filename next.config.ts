61f020035d77a08b94e1a5ad3ff2046c47475d4b
Prathamesh Dipak Thorat <92034832+Prathamesh625@users.noreply.github.com>
2025-09-25 16:42:40 +0530

    portfolio updated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/prathamesh.dev' : ''
   /* config options here */
 };
 
